a00ab46be7b66e8331e4eab1ad81918fc520f69f
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-01-20 21:23:24 +0000

    helper scripts added

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const requestLogger = (request, response, next) => {
 app.use(requestLogger)
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+    response.send('<h1>Unnecessary</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
