07b9ea3527635f578f1e73b3e6db01f14ce3de47
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-01-23 13:53:29 +0000

    getting all and single resources from datadase

--- index.js
+++ index.js
@@ -8,23 +8,6 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
@@ -45,28 +28,23 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notess => {
-        response.json(notess)
+    Note.find({}).then(notes => {
+        response.json(notes)
     })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(n => n.id === id)
-    
-    if (note) {
-        response.json(note);
-    } else {
-        response.status(404).end()
-    }
+    Note.findById(request.params.id).then(note => {
+        response.json(note)
+    })
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map((n) => n.id))
-        : 0;
-    return maxId + 1
-}
+// const generateId = () => {
+//     const maxId = notes.length > 0
+//         ? Math.max(...notes.map((n) => n.id))
+//         : 0;
+//     return maxId + 1
+// }
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -77,23 +55,22 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId(),
-    }
+        important: body.important || false,
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(n => n.id !== id)
+// app.delete('/api/notes/:id', (request, response) => {
+//     const id = Number(request.params.id)
+//     notes = notes.filter(n => n.id !== id)
 
-    response.status(204).end()
-})
+//     response.status(204).end()
+// })
 
 app.use(unknownEndpoint)
 
