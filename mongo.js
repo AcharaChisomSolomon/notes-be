2370aa8c6f7339fb7894348257548abaf5f017be
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-01-26 16:24:04 +0000

    supertest for web data testing

--- mongo.js
+++ mongo.js
@@ -1,13 +1,7 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
-}
-
-const password = process.argv[2]
-
-const url = `mongodb+srv://notes-fullstack:${password}@cluster0.dcqjixz.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = process.env.TEST_MONGODB_URI
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
@@ -20,14 +14,16 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model('Note', noteSchema)
 
 // const note = new Note({
-//     content: 'CSS is a pain in the ass',
-//     important: false,
+//     content: "",
+//     important: true,
 // })
 
-// note.save().then(result => {
-//     console.log('note saved!')
-//     mongoose.connection.close()
-// })
+// note.save()
+//     .then(() => {
+//         console.log('note saved!')
+//         mongoose.connection.close()
+//     })
+
 
 Note.find({}).then(result => {
     result.forEach(n => {
