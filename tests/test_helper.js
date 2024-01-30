52f8aec0f17832f7e8c8273e46d31c32a3ae2db0
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-01-30 13:28:27 +0000

    integrated tests added for the router functionalities

---
+++ tests/test_helper.js
@@ -0,0 +1,29 @@
+const Note = require('../models/note')
+
+const initialNotes = [
+    {
+        content: "HTML IS easy",
+        important: false,
+    },
+    {
+        content: "Browser can execute only JavaScript",
+        important: true,
+    },
+];
+
+const nonExistingId = async () => {
+    const note = new Note({ content: 'willremovethissoon' })
+    await note.save()
+    await note.deleteOne()
+
+    return note._id.toString()
+}
+
+const notesInDB = async () => {
+    const notes = await Note.find({})
+    return notes.map(n => n.toJSON())
+}
+
+module.exports = {
+    initialNotes, nonExistingId, notesInDB
+}
